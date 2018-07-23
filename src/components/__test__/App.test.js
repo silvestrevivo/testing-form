e3cd926606532f192038781628b3d970f448ba7f
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-23 10:41:19 +0200

    enzyme installed with adapter for React16

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from '../App'
+import CommentBox from '../CommentBox'
 
 it('shows a comment box', () => {
   const div = document.createElement('div')
 
   ReactDOM.render(<App />, div)
 
-  expect(div.innerHTML).toContain('Comment Box')
+  // expect(div.innerHTML).toContain('Comment Box') wrong approach
+  expect(div).toHaveAnInstaceOf(CommentBox)
 
   ReactDOM.unmountComponentAtNode(div)
 })
