7f22e07ec30e536115db0ed08e8f8b28f42dd255
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-23 11:34:05 +0200

    testing with Enzyme, refractoring the code

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 
 import App from '../App'
 import CommentBox from '../CommentBox'
 
 it('shows a comment box', () => {
-  const div = document.createElement('div')
+  const wrapped = shallow(<App />)
 
-  ReactDOM.render(<App />, div)
-
-  // expect(div.innerHTML).toContain('Comment Box') wrong approach
-  expect(div).toHaveAnInstaceOf(CommentBox)
-
-  ReactDOM.unmountComponentAtNode(div)
+  expect(wrapped.find(CommentBox).length).toEqual(1)
 })
