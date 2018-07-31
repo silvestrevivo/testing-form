042d7bd6534664c2f83783fcc14a28113eafb852
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-31 21:27:51 +0200

    adding first test to reducers

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,15 @@
+import commentsReducers from 'reducers/comments'
+import { SAVE_COMMENT } from 'actions/types'
+
+it('handles action of type SAVE_COMMENT', () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: 'New Comment'
+  }
+
+  const newState = commentsReducers([], action)
+
+  expect(newState).toEqual(['New Comment'])
+})
+
+
