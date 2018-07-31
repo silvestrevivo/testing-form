3516b314e2dea102f3e392b746a6ed327a2aeea7
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-31 21:31:25 +0200

    testing reducer with unknown type

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -13,3 +13,10 @@ it('handles action of type SAVE_COMMENT', () => {
 })
 
 
+it('handles action with unknown type', () => {
+
+  const newState = commentsReducers([], {type: 'unknown'});
+
+  expect(newState).toEqual([]);
+})
+
