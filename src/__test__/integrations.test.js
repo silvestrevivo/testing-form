e559daae7fbb27f8713440f205b327df913419c2
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-24 14:58:04 +0200

    moxios working at integration test but failing

--- src/__test__/integrations.test.js
+++ src/__test__/integrations.test.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React from 'react'
 import { mount } from 'enzyme'
-import Root from 'Root'
-import App from 'components/App';
+import moxios from 'moxios'
+import Root from 'root'
+import App from 'components/App'
+
+beforeEach(() => {
+  moxios.install()
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }],
+  })
+})
+
+afterEach(() => {
+  moxios.uninstall()
+})
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the entire app
   const wrapped = mount(
     <Root>
       <App />
-    </Root>
+    </Root>,
   )
 
   // find the 'fetchComments' button and click it
-  wrapped.find('.fetch-comments').simulate('click');
+  wrapped.find('.fetch-comments').simulate('click')
 
   // Expect to find a list of comments!
-  expect(wrapped.find('li').length).toEqual(500);
+  expect(wrapped.find('li').length).toEqual(2)
 })
-
