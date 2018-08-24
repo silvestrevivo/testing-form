6e3f230bea5c99cc82b1f09b56d3b14ea7a52926
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-24 15:19:15 +0200

    moxios.wait function added to replace setTimeout

--- src/__test__/integrations.test.js
+++ src/__test__/integrations.test.js
@@ -28,10 +28,11 @@ it('can fetch a list of comments and display them', () => {
   wrapped.find('.fetch-comments').simulate('click')
 
   // We are gonna introduce a small pause to solve the asynchronous behaviour
-  setTimeout(() => {
+  moxios.wait(() => {
     // Expect to find a list of comments!
     expect(wrapped.find('li').length).toEqual(2)
 
     done()
+    wrapped.unmount()
   }, 100)
 })
